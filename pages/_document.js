12570316c84ecc3664882d7f24e71b3c13b0a78f
cryalonebaby <cryalonebabyyy@gmail.com>
2022-11-02 16:28:04 +0200

    Fixed Color Mode

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
-import {newTheme} from '../styles/theme'
 
 export default function Document() {
   return (
     <Html>
       <Head/>
       <body>
-        <ColorModeScript initialColorMode={newTheme.initialColorMode} />
         <Main />
         <NextScript />
       </body>
